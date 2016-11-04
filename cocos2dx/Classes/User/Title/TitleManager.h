9a20d43a29376d192b4ac33a83f6ed534fe9abbf
R.komatsu <majaj62525@icloud.com>
2016-11-04 14:22:23 +0900

    add new file and file update

---
+++ cocos2dx/Classes/User/Title/TitleManager.h
@@ -0,0 +1,30 @@
+# ifndef __TitleManager__
+# define __TitleManager__
+
+#include "../LayerBase.h"
+
+namespace User
+{
+    class TitleManager : public LayerBase
+    {
+    public:
+        TitleManager( );
+        ~TitleManager( );
+		CREATE_FUNC(TitleManager);
+    private:
+
+		bool init();
+		void createTitleWindow();
+		void createTapUI();
+		void createFadeSprite();
+		
+
+		cocos2d::Sprite* fadeSprite;
+		cocos2d::Label*  uiLabel;
+		cocos2d::Action* normalAction();
+		cocos2d::Action* afterAction();
+		cocos2d::Action* fadeAction(float _fadeTime, float _delayTime);
+    };
+}
+
+# endif // __TitleManager__
