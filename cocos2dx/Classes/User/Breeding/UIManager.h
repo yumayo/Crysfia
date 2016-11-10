871d960c55a30ea899a027babcf427299f5a2962
R.komatsu <majaj62525@icloud.com>
2016-11-10 16:22:31 +0900

    メニュー更新
    メニューウィンドウの使用変更
    育成画面に切り替えた時の背景の変更
    育成画面からメイン画面に戻ってきた時の背景の変更

--- cocos2dx/Classes/User/Breeding/UIManager.h
+++ cocos2dx/Classes/User/Breeding/UIManager.h
@@ -3,6 +3,7 @@
 
 # include "cocos2d.h"
 #include "../LayerBase.h"
+#include "BGManager.h"
 #include "ui/CocosGUI.h"
 #include "User/Breeding/VariousStatus.h"
 
@@ -13,7 +14,7 @@ namespace User
 	private:
 
 		bool isOpen;
-		void update(float dt);
+		//void update(float dt);
 		bool init();
 
 	public:
@@ -23,27 +24,28 @@ namespace User
 		~UIManager();
 
 	private:
-
-		std::string buttonTexts[MenuType::MAX] = {
-			"STO\n RY",
-			"BREE\nDING",
-			"DIA\n RY",
-			"OPT\nION"
-		};
-
+		BGManager* bgManager;
 		cocos2d::ui::ListView*	menuWindow;
 		cocos2d::ui::ListView*	optionWindow;
 		cocos2d::ui::Button*	swicthWindow;
 		cocos2d::ui::Button*	optionIcon;
 
 		std::vector<cocos2d::ui::Button*> menuButtons;
+		std::vector<cocos2d::ui::Button*> subButtons;
 		std::vector<cocos2d::ui::Slider*> sliderBers;
 		std::vector<cocos2d::Label*>	  optionLabels;
-		
-		void touchEvent(Ref* pSender, cocos2d::ui::Widget::TouchEventType type);
-		void createMenuWindow();
+
+		void touchEventOfMainMenu(Ref* pSender, cocos2d::ui::Widget::TouchEventType type);
+		void touchEventOfSubMenu(Ref* pSender, cocos2d::ui::Widget::TouchEventType type);
+
+		void changeToMainWindow();
+		void changeToSubWindow();
+
+		void swapWindow(Node* moveOutObj, Node* moveInObj);
+
+		void createMaineMenuWindow();
+		void createSubMenuWindow();
 		void setOptionWindow();
-		void setDiaryWindow();
 	};
 }
 
