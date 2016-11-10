bd359c7b8e57b6dae259c4fc712b98cd0407e467
R.komatsu <majaj62525@icloud.com>
2016-11-10 18:53:13 +0900

    ファイル修正

--- cocos2dx/Classes/User/Breeding/VariousStatus.h
+++ cocos2dx/Classes/User/Breeding/VariousStatus.h
@@ -1,18 +1,48 @@
 #pragma once
 
 //描画順用ステータス
-enum zOder {
-	Menu = 1,
-	Option,
+enum class zOder {
+	BACKGROUND =  0,
+	MAIN_MENU,
+	SUB_MENU,
+	OPTION,
+};
+
+enum class LayerType
+{
+	BACGROUND = 0,
+	MAIN_MENU,
+	SUB_MENU,
+	DIARY_MENU,
+	OPTION,
+
+	MAX
 };
 
 //メニューステータス
 //追加するときはMAXの上に記述すること
-enum MenuType {
-	STORY,
+enum class ButtonType {
+	STORY = 0,
 	BREEDING,
 	DIARY,
 	OPTION,
+
+	MAX
+};
+
+enum class SubButtonType
+{
+	BACK,
+	MEAL,
+	CLOTHES,
+	CLEANING,
+
+	MAX
+};
+
+enum class BGType {
+	MAIN_MENU = 0,
+	BREEDING_MENU,
 
 	MAX
 };
